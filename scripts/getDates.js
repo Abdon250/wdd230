873b60b3f7e202fb2fd46d33f2bde6cb4f2885d4
Abdon250 <abdontuyizere@gmail.com>
2023-09-29 07:52:55 +0200

    main

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -4,3 +4,19 @@ let lastModify = new Date(document.lastModified);
 document.getElementById('currentyear').innerHTML = year;
 document.getElementById('lastModified').innerHTML = lastModify;
 
+
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+numVisits++;
+
+
+localStorage.setItem("numVisits-ls", numVisits);
+
