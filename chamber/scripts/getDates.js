091d09c960afed144ab03c5c2c625d08713d9c8a
Abdon250 <abdontuyizere@gmail.com>
2023-09-29 07:46:53 +0200

    discovery

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -29,3 +29,19 @@ todayElement.textContent = today;
 christmasElement.textContent = christmasDate.getTime();
 christmasDateElement.textContent = christmasDate;
 daysElement.textContent = `${daysleft.toFixed(0)} days`;
+
+
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+numVisits++;
+
+
+localStorage.setItem("numVisits-ls", numVisits);
